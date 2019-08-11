c95ebf026dc26f960c9b814c2664c2ca96d9e067
Kai Wohlfahrt <kai.wohlfahrt@gmail.com>
2019-08-11 21:56:12 +0100

    WIP: Try to make dom events work

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import * as rx from "rxjs";
 import * as op from "rxjs/operators";
 import { run } from "@cycle/rxjs-run";
-import { makeDOMDriver } from '@cycle/dom';
+import { makeDOMDriver, DOMSource } from '@cycle/dom'
 import * as Snabbdom from 'snabbdom-pragma';
 
-const main = () => {
+const main = (sources: {
+    DOM: DOMSource
+}) => {
+    const {DOM} = sources;
     const sinks = {
-	DOM: rx.interval(1000).pipe(
+	DOM: DOM.select("button").events("click").pipe(
 	    op.startWith(0),
 	    op.scan(acc => acc + 1),
-	    op.map(i => <h1>{`${i} seconds elapsed`}</h1>),
+	    op.map(i => <div>
+		<h1>{`clicked ${i} times`}</h1>
+		<button>Click me!</button>
+	    </div>
+	    ),
 	)
     };
     return sinks;
